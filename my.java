02e40662b1f58fe69c51cd70c76ff53d3a6e5fe5
vihanga02 <muthumalavdw.22@uom.lk>
2023-12-03 20:17:08 +0530

    nexted loops

--- my.java
+++ my.java
@@ -2,11 +2,28 @@ import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.Scanner;
 
 public class my {
-    public static void main(String[] args){
-        for (int i = 10; i > 0 ; i--)
-        {
-            System.out.println(i);
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int rows,columns;
+
+    String symbol = "";
+
+    System.out.println("enter rows");
+    rows = scanner.nextInt();
+
+    System.out.println("enter column: ");
+    columns = scanner.nextInt();
+
+    System.out.println("enter the symbol");
+    symbol = scanner.next();
+
+    for (int i = 0 ; i <= rows ; i++){
+        System.out.println();
+        for (int j = 0 ; j <= columns ; j++){
+            System.out.print(symbol);
         }
-        System.out.println("pala htto");
+    }
+
     }
 }
