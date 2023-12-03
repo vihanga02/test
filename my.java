c77cbc6aa00a5ab65811cbba39276c1f1e1a2cb6
vihanga02 <muthumalavdw.22@uom.lk>
2023-12-03 20:01:54 +0530

    for loops

--- my.java
+++ my.java
@@ -1,5 +1,12 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.Scanner;
+
 public class my {
     public static void main(String[] args){
-        System.out.println("fuck me");
+        for (int i = 10; i > 0 ; i--)
+        {
+            System.out.println(i);
+        }
+        System.out.println("pala htto");
     }
 }
