6eefc64566b9fb69645f71007053bb285ead28eb
vihanga02 <muthumalavdw.22@uom.lk>
2023-12-03 20:57:14 +0530

    2D array

--- my.java
+++ my.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 
 public class my {
     public static void main(String[] args) {
-         String[] cars = new String[3];
+        String[][] cars = new String[3][3];
 
-         cars[0] = "htta";
-         cars[1] = "pnnya";
-         cars[2] = "kkkk";
+        cars[0][0] = "pakya";
+        cars[0][1] = "htta";
+        cars[0][2] = "mmmm";
+        cars[1][0] = "httgpth";
+        cars[1][1] = "acgpth";
+        cars[1][2] = "kkkk";
+        cars[2][0] = "namasthe";
+        cars[2][1] = "namaskaram";
+        cars[2][2] = "aesgda";
 
-         for (int i =0 ; i < cars.length; i++){
-             System.out.println(cars[i]);
-         }
-
-
-
+        for (int i = 0 ; i < cars.length; i++){
+            System.out.println();
+            for (int j = 0; j<cars[1].length ; j++){
+                System.out.println(cars[i][j] + " ");
+            }
+        }
     }
 }
